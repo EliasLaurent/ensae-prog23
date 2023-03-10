82a4655a7f5fca82e71983b603a9e568667e1e4f
elaurent <elias.laurent@ensae.fr>
2023-03-10 14:31:35 +0000

    initialisation

---
+++ input/graph.py
@@ -0,0 +1,195 @@
+class Graph:
+    """
+    A class representing graphs as adjacency lists and implementing various algorithms on the graphs. Graphs in the class are not oriented. 
+    Attributes: 
+    -----------
+    nodes: NodeType
+        A list of nodes. Nodes can be of any immutable type, e.g., integer, float, or string.
+        We will usually use a list of integers 1, ..., n.
+    graph: dict
+        A dictionnary that contains the adjacency list of each node in the form
+        graph[node] = [(neighbor1, p1, d1), (neighbor2, p2, d2), ...]
+        where p1 is the minimal power on the edge (node, neighbor1) and d1 is the distance on the edge
+    nb_nodes: int
+        The number of nodes.
+    nb_edges: int
+        The number of edges. 
+    """
+
+    def __init__(self, nodes=[]):
+        """
+        Initializes the graph with a set of nodes, and no edges. 
+        Parameters: 
+        -----------
+        nodes: list, optional
+            A list of nodes. Default is empty.
+        """
+        self.nodes = nodes
+        self.graph = dict([(n, []) for n in nodes])
+        self.nb_nodes = len(nodes)
+        self.nb_edges = 0
+    
+
+    def __str__(self):
+        """Prints the graph as a list of neighbors for each node (one per line)"""
+        if not self.graph:
+            output = "The graph is empty"            
+        else:
+            output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
+            for source, destination in self.graph.items():
+                output += f"{source}-->{destination}\n"
+        return output
+    
+    def add_edge(self, node1, node2, power_min, dist=1):
+        """
+        Adds an edge to the graph. Graphs are not oriented, hence an edge is added to the adjacency list of both end nodes. 
+
+        Parameters: 
+        -----------
+        node1: NodeType
+            First end (node) of the edge
+        node2: NodeType
+            Second end (node) of the edge
+        power_min: numeric (int or float)
+            Minimum power on this edge
+        dist: numeric (int or float), optional
+            Distance between node1 and node2 on the edge. Default is 1.
+        """
+        if node1 not in self.graph:
+            self.graph[node1] = []
+            self.nb_nodes += 1
+            self.nodes.append(node1)
+        if node2 not in self.graph:
+            self.graph[node2] = []
+            self.nb_nodes += 1
+            self.nodes.append(node2)
+
+        self.graph[node1].append((node2, power_min, dist))
+        self.graph[node2].append((node1, power_min, dist))
+        self.nb_edges += 1
+    
+
+    def get_path_with_power(self, src, dest, power):
+        raise NotImplementedError
+    
+    def comp(self,l,lb,lv):
+        if lv==[]:
+            return(l,lb)
+        else:   
+            i=lv.pop()
+            if lb[i-1]:
+                lb[i-1]=False
+                b=[a for (a,a2,a3) in self.graph[i]]
+                l.append(i)
+                lv+=b
+            return(self.comp(l,lb,lv))
+            
+    def compco(self):
+        n=self.nb_nodes
+        lb=[True for i in range(n)]
+        c=[]
+        for k in range(1,n+1):
+            l1,l2=self.comp([],lb,[k])
+            lb=l2
+            if l1!=[]:
+                c.append(l1)
+        return(c)
+
+    def min_power(self, src, dest):
+        """
+        Should return path, min_power. 
+        """
+        raise NotImplementedError
+
+
+    def CCsommet(self,i):
+        n=len(self.nodes)
+        L=[]
+        compconn=[0 for  _ in range(n)]
+        compconn[i-1]=1
+        L+=self.graph[i]
+        while L!=[]:
+            voisin = L.pop(0)
+            if compconn[voisin[0]-1]==0 :
+                compconn[voisin[0]-1]=1
+                L+=self.graph[voisin[0]] #ajout dans L des voisins de voisin
+        return(compconn)
+
+    def CConnexes(self):
+        L=[]
+        for i in self.nodes:
+            L.append(self.CCsommet(i))
+        Lsansdouble = [] 
+        for i in L : 
+            if i not in Lsansdouble: 
+                Lsansdouble.append(i) 
+        return(Lsansdouble)
+#3
+    def get_path_with_power(self,p,t):
+        debut,fin=t[0],t[1]
+        n=len(self.nodes)
+        visit=[0 for _ in range(n)]
+        cc=self.compco()
+        for c in cc:
+            if c.count(debut)==1:
+                if c.count(fin)==0:
+                    return None
+        n=self.nb_nodes
+        M=[[debut]]
+        L=[debut]
+        visit[debut-1]=1
+        voisin=self.graph[debut]
+        while L!=[]:
+            m=[]
+            L.pop(0)
+            for trouple in voisin :
+                if visit[trouple[0]-1]==0 and trouple[1]<=p:
+                    m.append(trouple[0])
+                    visit[trouple[0]-1]=1
+                    L.append(trouple[0])
+            if m!=[]:
+                M.append(m)
+            if L!=[]:    
+                print(L[0])
+                voisin=self.graph[L[0]]
+        chemin=[]
+        ind=0
+        for k in M:
+            if fin in k:
+                ind=M.index(k)
+        b=fin
+        for l in range(ind,-1,-1):
+            if l==ind:
+                chemin.append(fin)
+            else:
+                b=M[l][0]
+                chemin.append(b)
+        if len(chemin)>1:
+            return(chemin)
+        else:
+            return(None)
+
+        
+def graph_from_file(filename):
+    with open(filename, "r") as file:
+        n, m = map(int, file.readline().split())
+        g = Graph(range(1, n+1))
+        for _ in range(m):
+            edge = list(map(int, file.readline().split()))
+            if len(edge) == 3:
+                node1, node2, power_min = edge
+                g.add_edge(node1, node2, power_min) # will add dist=1 by default
+            elif len(edge) == 4:
+                node1, node2, power_min, dist = edge
+                g.add_edge(node1, node2, power_min, dist)
+            else:
+                raise Exception("Format incorrect")
+    return g
+
+"""g = graph_from_file("work/network.01.in")
+print(g)
+print(g.compco()) test"""
+
+
+#TD2
+
---
+++ input/main.py
@@ -0,0 +1,9 @@
+from graph import Graph, graph_from_file
+
+
+data_path = "input/"
+file_name = "network.02.in"
+
+g = graph_from_file(data_path + file_name)
+print(g)
+print(g.get_path_with_power(12,(2,1)))
